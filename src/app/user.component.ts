46dba97300bf80d06a9885e2dbf507aa562a115a
Faiz <faiz.ggmu@gmail.com>
2018-08-05 21:25:43 +0500

    Two way binding directive for event handling

--- src/app/user.component.ts
+++ src/app/user.component.ts
@@ -3,9 +3,13 @@ import { Component } from '@angular/core';
   selector: 'app-user',
   template: `
   <p>Hello {{ name }} from another Component</p>
-  <input type="text" (input)="OnUserInput($event)">
-  `
+ <input type="text" [(ngModel)]="name" >
 
+  `
+  // [(ngModel)]="name" angular will now bind in both direction two way biniding /
+  // [value]="name" here we bind to the value property of the input element object
+  // value="name" we set the value the attirbute to html
+  //  <input type="text" (input)="OnUserInput($event)" value="{{ name }}">
 })
 export class UserComponent {
 name = 'faiz';
